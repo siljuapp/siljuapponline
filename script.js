2bf03f08c640e3c26bc33dcf0cffc51fa1fb773c
Mehboob Elahi <mehboob4ias@gmail.com>
2024-08-03 13:49:07 +0530

    ssss

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const storage = getStorage(app);
-
+// nothing
 var exam = "ssc";
 // gist data
 var que_data = [];
